42fcfa94080d761810e7cde47ee1bbe05ba3dc8d
pbevan1 <69802263+pbevan1@users.noreply.github.com>
2021-01-20 18:19:27 +0000

    Add files via upload

---
+++ titanic_ml_pbevan1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='CSC8635 Machine Learning - prediction of survival of the titanic disaster using a variety of machine learning models',
+    author='Peter Bevan',
+    license='',
+)
